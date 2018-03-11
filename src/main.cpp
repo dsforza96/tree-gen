98fabc278d2d7cc477660212eee80328c637a1b8
dsforza <sforza.1710262@studenti.uniroma1.it>
2018-03-11 15:44:52 +0100

    Completata funzione throw_darts

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,42 @@
-#include "../src/yocto/yocto_gl.h"
-#include "../src/yocto/yocto_gl.cpp"
+#include "yocto/yocto_gl.h"
+#include "yocto/yocto_gl.cpp"
+#include "cppdelaunay/delaunay/Voronoi.h"
+#include "cppdelaunay/delaunay/Voronoi.cpp"
 
 using namespace ygl;
 
+// Genera in modo randomico i punti di attrazione
+shape* throw_darts(int N, vec2f p0, vec2f p1, vec2f t0, vec2f t1)
+{
+    auto shp = new shape{"points"};
 
+    auto rng = init_rng(time(nullptr));
+
+    for (auto i = 0; i < N; i++)
+    {
+        auto y = next_rand1f(rng, p0.x, p1.x);
+        auto p = eval_bezier_cubic(p1, t1, t0, p0, y);
+        auto xz = next_rand2f(rng, -p, p);
+
+        shp->pos.push_back({xz.x, y, xz.y)});
+    }
+
+    return shp;
+}
 
 int main()
 {
+    auto N = 10;
     auto p0 = vec2f{0, 0};
     auto p1 = vec2f{1, 0};
     auto t0 = vec2f{0.6, 0.2};
     auto t1 = vec2f{0.1, 0.4};
+    auto root = vec3f{0, 0, 0};
 
     // vec2f x = eval_bezier_cubic(p1, t1, p0, t0, 0); py confronto
 
-    for (auto i = 0; i < 100; i++)
-    {
-        auto x = i / 100.0f;
-        vec2f p = eval_bezier_cubic(p1, t1, t0, p0, x);
-        for (auto j = 0; j < (int) (p.y * 100); j++)
-        {
-            printf(".");
-        }
-        printf("\n");
-    }
+    auto points = throw_darts(N, p0, p1, t0, t1);
+
 
 
     return 0;
