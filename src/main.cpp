1b7d5b5b558aaa4a66c587230a32511f37d25a4f
dsforza <sforza.1710262@studenti.uniroma1.it>
2018-03-14 21:29:53 +0100

    Sistemato cmake per voro++. Creato diagramma di Voronoi

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,20 @@
 #include "yocto/yocto_gl.h"
 #include "yocto/yocto_gl.cpp"
-#include "cppdelaunay/delaunay/Voronoi.h"
-#include "cppdelaunay/delaunay/Voronoi.cpp"
+#include "voro++-0.4.6/src/voro++.hh"
 
 using namespace ygl;
 
 // Genera in modo randomico i punti di attrazione
-shape* throw_darts(int N, vec2f p0, vec2f p1, vec2f t0, vec2f t1)
+voro::container* throw_darts(int N, vec2f p0, vec2f p1, vec2f t0, vec2f t1)
 {
-    auto shp = new shape{"points"};
+    auto points = std::vector<vec3f>();
+    auto xmax = 0.0f;
+    auto xmin = 0.0f;
+    auto zmax = 0.0f;
+    auto zmin = 0.0f;
 
     auto rng = init_rng(time(nullptr));
+
 
     for (auto i = 0; i < N; i++)
     {
@@ -18,15 +22,28 @@ shape* throw_darts(int N, vec2f p0, vec2f p1, vec2f t0, vec2f t1)
         auto p = eval_bezier_cubic(p1, t1, t0, p0, y);
         auto xz = next_rand2f(rng, -p, p);
 
-        shp->pos.push_back({xz.x, y, xz.y)});
+        points.push_back({xz.x, y, xz.y});
+
+        xmax = max(xmax, p.x);
+        xmin = min(xmin, p.x);
+        zmax = max(zmax, p.y);
+        zmin = min(zmin, p.y);
     }
 
-    return shp;
+    auto vorodiag = new voro::container(xmin, xmax, p0.x, p1.x, zmin, zmax, N / 5, N / 5, N / 5, false, false, false, 8);
+
+    for (auto i = 0; i < N; i++)
+        vorodiag->put(i, points[i].x, points[i].y, points[i].z);
+
+    return vorodiag;
 }
+
+void grow();
 
 int main()
 {
     auto N = 10;
+    auto iter_num = 100;
     auto p0 = vec2f{0, 0};
     auto p1 = vec2f{1, 0};
     auto t0 = vec2f{0.6, 0.2};
@@ -35,9 +52,14 @@ int main()
 
     // vec2f x = eval_bezier_cubic(p1, t1, p0, t0, 0); py confronto
 
-    auto points = throw_darts(N, p0, p1, t0, t1);
+    auto vorodiag = throw_darts(N, p0, p1, t0, t1);
 
-
+    for (auto i = 0; i < iter_num; i++)
+    {
+        auto pid = 0;
+        auto x = 0.0d, y = 0.0d, z = 0.0d;
+        vorodiag->find_voronoi_cell(root.x, root.y, root.z, x, y, z, pid);
+    }
 
     return 0;
 }
